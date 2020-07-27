8fffff6ecc796e94798e24b9039bcbc3213c42cf
Grant Holley <gvholley@gmail.com>
2020-07-27 21:21:26 +0900

    Added NewRecipe

---
+++ app/javascript/components/NewRecipe.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class NewRecipe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      ingredients: "",
+      instruction: ""
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.stripHtmlEntities = this.stripHtmlEntities.bind(this);
+  }
+
+  stripHtmlEntities(str) {
+    return String(str)
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;");
+  }
+
+}
+
+export default NewRecipe;
